8d8f33f85bae89550467e363bcdf039fd4012a99
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-08 15:22:56 -0600

    modified baselines

--- deliverable_1/baselines.py
+++ deliverable_1/baselines.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append( '..' )
 
-from loader import *
+from loader import * #Import the helper load functions
 
 if __name__ == '__main__':
     x = load_json_tweets('../tweets.json', 100)
