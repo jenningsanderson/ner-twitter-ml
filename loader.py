ab1399e55e2b88d4590aba25d0566352140f7214
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-08 15:06:19 -0600

    wrote a small python script to load the tweet json

---
+++ loader.py
@@ -0,0 +1,24 @@
+import json
+
+
+def load_json_tweets(file, limit=None):
+    """
+    Load the JSON file of tweets
+
+    :param file:  The location of the json file as a string
+    :param limit: Only return the first <n> tweets
+    """
+    tweets = []
+
+    i = 0
+    for id, tweet in json.load( open(file) ).iteritems():
+        if i<limit or limit==None:
+            tweets.append(tweet)
+            i+=1
+
+    return tweets
+
+
+if __name__ == '__main__':
+    x = load_json_tweets('tweets.json', 10)
+    print len(x)
