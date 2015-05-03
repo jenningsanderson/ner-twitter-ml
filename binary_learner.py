1478048942e5d22b618a3d02888c75f0ccb4da69
Jennings Anderson <jennings.anderson@colorado.edu>
2015-05-02 18:56:02 -0600

    updating print statements

--- binary_learner.py
+++ binary_learner.py
@@ -66,22 +66,27 @@ class Entity:
             self.y_test_arr.append(  [ self.data[i]['label'] for i in test_index ]  )
 
     def do_full_svm(self):
-        self.accuracies = []
-        self.precisions = []
-        self.recalls    = []
-        self.f1s        = []
+        self.accuracies = [0]*self.iterations
+        self.precisions = [0]*self.iterations
+        self.recalls    = [0]*self.iterations
+        self.f1s        = [0]*self.iterations
         for i in range(self.iterations):
             self.clf = LinearSVC()
             self.clf.fit(self.x_train_arr[i], self.y_train_arr[i])
-            svm_predictions = self.clf.predict(self.x_test_arr[i])
-            self.precisions.append( metrics.precision_score(y_true=self.y_test_arr[i], y_pred=svm_predictions, pos_label=None) )
-            self.recalls.append( metrics.recall_score(y_true=self.y_test_arr[i], y_pred=svm_predictions, pos_label=None) )
-            self.f1s.append( metrics.f1_score(y_true=self.y_test_arr[i], y_pred=svm_predictions, pos_label=None) )
-            self.accuracies.append(metrics.accuracy_score(self.y_test_arr[i], svm_predictions))
+            self.svm_prediction = self.clf.predict(self.x_test_arr[i])
+
+            self.precisions[i] = metrics.precision_score(y_true=self.y_test_arr[i], y_pred=self.svm_prediction, pos_label=None)
+            self.recalls[i]    = metrics.recall_score(y_true=self.y_test_arr[i], y_pred=self.svm_prediction, pos_label=None)
+            self.f1s[i]        = metrics.f1_score(y_true=self.y_test_arr[i], y_pred=self.svm_prediction, pos_label=None)
+            self.accuracies[i] = metrics.accuracy_score(self.y_test_arr[i], self.svm_prediction)
+
+            print "Metrics Classification for " + self.name + "; Round: " + str(i)
+            print metrics.classification_report(y_true=self.y_test_arr[i], y_pred=self.svm_prediction)
+            
 
 if __name__ == "__main__":
 
-    limit       = None  #Limit of rows to load from the CSV
+    limit       = 3000  #Limit of rows to load from the CSV
     iterations  = 5     #Number of iterations for the cross validation
 
     words = loader.load_csv_tweets('./data/LIWC2001Results_5class.csv', limit=limit)
@@ -111,8 +116,8 @@ if __name__ == "__main__":
     #Print the results
     for ent in entities:
         print ent.name + "\n---------------------------"
-        print "Accuracy:  ", np.average(ent.accuracies)
-        print "Recall:    ", np.average(ent.recalls)
-        print "Precision: ", np.average(ent.precisions)
-        print "F1-Score:  ", np.average(ent.f1s)
+        print "Accuracy:  "+  str(ent.accuracies) + "; average: " + str(np.average(ent.accuracies) )
+        print "Recall:    "+  str(ent.recalls)    + "; average: " + str(np.average(ent.recalls)    )
+        print "Precision: "+  str(ent.precisions) + "; average: " + str(np.average(ent.precisions) )
+        print "F1-Score:  "+  str(ent.f1s)        + "; average: " + str(np.average(ent.f1s)        )
         print "==========================="
